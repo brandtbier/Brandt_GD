3614d5b76cd8a03cbdcdddebf4aba2c578a98cdd
brandtbier <97189769+brandtbier@users.noreply.github.com>
2022-05-17 09:11:14 -0500

    Add files via upload

---
+++ Brandt_GD/Final/menu.py
@@ -0,0 +1,557 @@
+#MAria I SUarez
+#learning how to draw circles and rectangles
+#use keys to move objects
+#Using Dictionaries
+
+#Objective of the game is for the rect to run away fom the circle, if they collide the circle etas the square, 
+#circle will  get larger, and a new rect should appear somewhere on the screen
+# K_UP                  up circle
+# K_DOWN                down circle
+# K_RIGHT               right circle
+# K_LEFT                left circle
+# K_a                   left square
+# K_d                   right square
+# K_w                   up square
+# K_s                   down square
+# K_SPACE               jump
+#initialize pygame
+
+import os, random, time, pygame, math, datetime
+from turtle import screensize
+os.system('cls')
+name=input("What is your name? ")
+#initialize pygame
+pygame.init()
+
+#Declare constants, variables, list, dictionaries, any object
+#scree size
+WIDTH=700
+HEIGHT=700
+xMs=50
+yMs=250
+wb=30
+hb=30
+MAIN=True
+INST=False
+SETT=False
+LEV_I=False
+LEV_II=False
+LEV_III=False
+SCORE=False
+#List f messages
+MenuList=['Instructions','Settings', "Level I","Level II",'Level III','Scoreboard','Exit']
+SettingList=['Screen Size','Backgrnd Color','Icon','']
+sizeList=['1000 x 1000','800 x 800','600 x 600']
+check=True #for the while loop
+
+#create screen
+screen=pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption('Circle eats Square')
+
+#define colors
+colors={'white':[255,255,255], 'red':[255,0,0], 'aqua':[102,153, 255],
+'orange':[255,85,0],'purple':[48,25,52],'navy':[5,31,64],'pink':[200,3,75]}
+#Get colors
+background= colors.get('white')
+randColor=''
+cr_color=colors.get('aqua')
+sqM_color=colors.get('pink')
+BLACK=(0,0,0)
+#create fifferent type 
+TITLE_FNT=pygame.font.SysFont('comicsans', 80)
+MENU_FNT=pygame.font.SysFont('comicsans', 40)
+INST_FNT=pygame.font.SysFont('comicsans', 30)
+#Create square fr menu
+
+squareM=pygame.Rect(xMs,yMs,wb,hb)
+#Create Title
+def TitleMenu(Message):
+    text=TITLE_FNT.render(Message, 1, (255,0,0))
+    screen.fill((255,255,255))
+    #get the width  the text 
+    #x value = WIDTH/2 - wText/2
+    xt=WIDTH/2-text.get_width()/2
+    screen.blit(text,(xt,50))
+#This is a function uses a parameter
+def MainMenu(Mlist):
+    txty=243
+    squareM.y=250
+    for i in range(len(Mlist)):
+        message=Mlist[i]
+        text=INST_FNT.render(message,1,(51,131,51))
+        screen.blit(text,(90,txty))
+        pygame.draw.rect(screen,sqM_color, squareM )
+        squareM.y +=50
+        txty+=50
+    pygame.display.update()
+    pygame.time.delay(10)
+def changeColor():
+    global randColor
+    colorCheck=True
+    while colorCheck:
+        randColor=random.choice(list(colors))
+        if colors.get(randColor)==background:
+            print(randColor)
+            print(background)
+            randColor=random.choice(list(colors))
+        else:
+            colorCheck=False
+def instr():
+    myFile=open('Final Stretch\instructions.txt')
+    yi=150 
+    
+    stuff= myFile.readlines()
+    print(stuff)
+    for line in stuff:
+        print(line)
+        text=INST_FNT.render(line, 1, BLACK)
+        yi=150 
+        screen.blit(text, (40,yi))
+        pygame.display.update()
+        pygame.time.delay(50)
+        yi+=50
+    myFile.close()
+def keepScore(score):
+    date=datetime.datetime.now()
+    print(date.strftime('%m/%d/%Y'))
+    scoreLine=str(score)+"\t"+name+"\t"+date.strftime('%m/%d/%Y'+'\n')
+ 
+    #open a file and write in it 
+    # when y write it erases the prev 
+    myFile=open('ClassStuff\sce.txt','a') 
+    myFile.write(scoreLine)
+    myFile.close()
+def scoreBoard():
+    myFile=open('ClassStuff\CircleEatsSquare\sce.txt', 'r')
+    yi=150
+    stuff= myFile.readlines()
+    myFile.close()
+    stuff.sort()
+    N=len(stuff)-1
+    temp=[]
+    j=0
+    for i in range(N, -1, -1):
+        print(i,stuff[i])
+        # temp[j]=stuff[i]
+        #     j +=1
+        # print(temp)
+        # for i in range(N):
+        #     text=INST_FNT.render(temp[i], 1, BLACK)
+        #     screen.blit(text, (40,yi))
+        #     pygame.display.update()
+        #     pygame.time.delay(50)
+        #     yi+=50
+    
+def keepScore(score):
+    date=datetime.datetime.now()
+    print(date.strftime('%m/%d/%Y'))
+    scoreLine='\n'+str(score)+"\t"+name+"\t"+date.strftime('%m/%d/%Y'+'\n')
+ 
+    #open a file and write in it 
+    # when y write it erases the prev 
+    myFile=open('ClassStuff\CircleEatsSquare\sce.txt','a') 
+    myFile.write(scoreLine)
+    myFile.close()
+def changeScreenSize(xm,ym):
+    global HEIGHT, WIDTH, screen
+    if ((xm >20 and xm <80) and (ym >250 and ym <290)):
+        HEIGHT=1000
+        WIDTH=1000
+
+    if ((xm >20 and xm <80) and (ym >300 and ym <330)):
+        HEIGHT=800
+        WIDTH=800
+        
+    if ((xm >20 and xm <80) and (ym >350 and ym <380)):
+        HEIGHT=600
+        WIDTH=600
+    screen=pygame.display.set_mode((WIDTH,HEIGHT))
+def playGame2():
+    background = pygame.image.load('Final Stretch\Temp-800x600.jpg')
+    pygame.display.set_caption("Detonate")
+
+
+    # Player
+    playerImg= pygame.image.load('Final Stretch\gun.png')
+    playerX_change = 0
+    playerX = 370
+    playerY = 480
+    playerX_change = 0
+    # Enemy
+    enemyImg = []
+    enemyX = []
+    enemyY = []
+    enemyX_change = []
+    enemyY_change = []
+    num_of_enemies = 6
+
+    for i in range(num_of_enemies):
+        enemyImg.append(pygame.image.load('Final Stretch\Bwomb.jpg'))
+        enemyX.append(random.randint(0, 736))
+        enemyY.append(random.randint(50, 150))
+        enemyX_change.append(4)
+        enemyY_change.append(40)
+
+
+
+    bulletImg = pygame.image.load('Final Stretch\\bullet.png')
+    bulletX = 0
+    bulletY = 480
+    bulletX_change = 0
+    bulletY_change = 10
+    bullet_state = "ready"
+    key=pygame.key.get_pressed()
+    # Score
+
+    score_value = 0
+    font = pygame.font.Font('freesansbold.ttf', 32)
+
+    textX = 10
+    testY = 10
+
+    # Game Over
+    over_font = pygame.font.Font('freesansbold.ttf', 64)
+
+
+    def show_score(x, y):
+        score = font.render("Score : " + str(score_value), True, (255, 255, 255))
+        screen.blit(score, (x, y))
+
+
+    def game_over_text():
+        over_text = over_font.render("GAME OVER", True, (255, 255, 255))
+        screen.blit(over_text, (200, 250))
+
+
+    def player(x, y):
+        screen.blit(playerImg, (x, y))
+
+
+    def enemy(x, y, i):
+        screen.blit(enemyImg[i], (x, y))
+
+
+    def fire_bullet(x, y):
+        global bullet_state
+        bullet_state = "fire"
+        screen.blit(bulletImg, (x + 16, y + 10))
+
+
+    def isCollision(enemyX, enemyY, bulletX, bulletY):
+        distance = math.sqrt(math.pow(enemyX - bulletX, 2) + (math.pow(enemyY - bulletY, 2)))
+        if distance < 27:
+            return True
+        else:
+            return False
+
+def playGame():
+    background = pygame.image.load('Final Stretch\Temp-800x600.jpg')
+    pygame.display.set_caption("Detonate")
+
+    # Player
+    playerImg= pygame.image.load('Final Stretch\gun.png')
+    playerX_change = 0
+    playerX = 370
+    playerY = 480
+    playerX_change = 0
+    # Enemy
+    enemyImg = []
+    enemyX = []
+    enemyY = []
+    enemyX_change = []
+    enemyY_change = []
+    num_of_enemies = 4
+
+    for i in range(num_of_enemies):
+        enemyImg.append(pygame.image.load('Final Stretch\Bwomb.jpg'))
+        enemyX.append(random.randint(0, 736))
+        enemyY.append(random.randint(50, 150))
+        enemyX_change.append(2)
+        enemyY_change.append(30)
+
+
+
+    bulletImg = pygame.image.load('Final Stretch\\bullet.png')
+    bulletX = 0
+    bulletY = 480
+    bulletX_change = 0
+    bulletY_change = 10
+    bullet_state = "ready"
+    key=pygame.key.get_pressed()
+    # Score
+
+    score_value = 0
+    font = pygame.font.Font('freesansbold.ttf', 32)
+
+    textX = 10
+    testY = 10
+
+    # Game Over
+    over_font = pygame.font.Font('freesansbold.ttf', 64)
+
+
+    def show_score(x, y):
+        score = font.render("Score : " + str(score_value), True, (255, 255, 255))
+        screen.blit(score, (x, y))
+
+
+    def game_over_text():
+        over_text = over_font.render("GAME OVER", True, (255, 255, 255))
+        screen.blit(over_text, (200, 250))
+
+
+    def player(x, y):
+        screen.blit(playerImg, (x, y))
+
+
+    def enemy(x, y, i):
+        screen.blit(enemyImg[i], (x, y))
+
+
+    def fire_bullet(x, y):
+        global bullet_state
+        bullet_state = "fire"
+        screen.blit(bulletImg, (x + 16, y + 10))
+
+
+    def isCollision(enemyX, enemyY, bulletX, bulletY):
+        distance = math.sqrt(math.pow(enemyX - bulletX, 2) + (math.pow(enemyY - bulletY, 2)))
+        if distance < 27:
+            return True
+        else:
+            return False
+
+
+    # Game Loop
+    running = True
+    while running:
+
+        # RGB = Red, Green, Blue
+        screen.fill((0, 0, 0))
+        # Background Image
+        screen.blit(background, (0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            # if keystroke is pressed check whether its right or left
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    playerX_change = -3
+                if event.key == pygame.K_RIGHT:
+                    playerX_change = 3 
+                if event.key == pygame.K_SPACE:
+                    if bullet_state is "ready":
+
+                        # Get the current x cordinate of the spaceship
+                        bulletX = playerX
+                        fire_bullet(bulletX, bulletY)
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    playerX_change = 0
+
+        # 5 = 5 + -0.1 -> 5 = 5 - 0.1
+        # 5 = 5 + 0.1
+
+        playerX += playerX_change
+        if playerX <= 0:
+            playerX = 0
+        elif playerX >= 736:
+            playerX = 736
+
+        # Enemy Movement
+        for i in range(num_of_enemies):
+
+            # Game Over
+            if enemyY[i] > 440:
+                for j in range(num_of_enemies):
+                    enemyY[j] = 2000
+                game_over_text()
+                break
+
+            enemyX[i] += enemyX_change[i]
+            if enemyX[i] <= 0:
+                enemyX_change[i] = 4
+                enemyY[i] += enemyY_change[i]
+            elif enemyX[i] >= 736:
+                enemyX_change[i] = -4
+                enemyY[i] += enemyY_change[i]
+
+            # Collision
+            collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY)
+            if collision:
+
+                bulletY = 480
+                bullet_state = "ready"
+                score_value += 1
+                enemyX[i] = random.randint(0, 736)
+                enemyY[i] = random.randint(50, 150)
+
+            enemy(enemyX[i], enemyY[i], i)
+
+        # Bullet Movement
+        if bulletY <= 0:
+            bulletY = 480
+            bullet_state = "ready"
+
+        if bullet_state is "fire":
+            fire_bullet(bulletX, bulletY)
+            bulletY -= bulletY_change
+
+        player(playerX, playerY)
+        show_score(textX, testY)
+        pygame.display.update()
+
+
+#sq_color=colors.get('navy')
+#Making a rand c f the square
+changeColor()
+
+#==============================================
+#
+#Beginning  main prram
+sq_color=colors.get(randColor)
+keys=pygame.key.get_pressed()
+mouse_pos=(0,0)
+screCk=True
+first=True
+xm=0 
+ym=0
+f_SEET=True
+sc_size=False
+set_first=True
+c_first=True
+while check:
+    for case in pygame.event.get():
+        if case.type==pygame.QUIT:
+            check=False
+        if case.type ==pygame.MOUSEBUTTONDOWN:
+            mouse_pos=pygame.mouse.get_pos()
+            xm= mouse_pos[0]
+            ym= mouse_pos[1]
+        # print(mouse_pos)
+    keys=pygame.key.get_pressed() #this returns a list
+    if MAIN:
+        screen.fill(background)
+        TitleMenu("MENU")
+        MainMenu(MenuList)
+    if INST and first:
+        screen.fill(background)
+        TitleMenu("INSTRUCTIONS")
+        instr()
+        
+        
+        first=False
+    if INST:
+        if keys[pygame.K_ESCAPE]:
+            INST=False
+            MAIN=True
+            first=True
+    if SETT and f_SEET:
+        screen.fill(background)
+        TitleMenu("SETTINGS")
+        MainMenu(SettingList)
+        f_SEET=False
+    if SETT:
+        if keys[pygame.K_ESCAPE]:
+            SETT=False
+            MAIN=True
+            f_SEET=True
+    if LEV_I:
+        screen.fill(background)
+        playGame()
+        LEV_I=False
+        MAIN=True
+        xm=0
+        ym=0
+    if LEV_II:
+        screen.fill(background)
+        playGame2()
+        LEV_II=False
+        MAIN=True
+    if LEV_III:
+        screen.fill(background)
+        TitleMenu("LEVEL III")
+        if keys[pygame.K_ESCAPE]:
+            LEV_III=False
+            MAIN=True
+    if SCORE and screCk:
+        screen.fill(background)
+        TitleMenu("SCOREBOARD")
+        scoreBoard()
+        #call funct t print scres
+        screCk=False
+    if SCORE:
+        if keys[pygame.K_ESCAPE]:
+            SCORE=False
+            MAIN=True
+            screCk=True
+    if ((xm >20 and xm <80) and (ym >250 and ym <290)) and MAIN:
+        MAIN=False
+        INST=True
+    if ((xm >20 and xm <80) and (ym >300 and ym <330))and MAIN:
+        MAIN=False
+        SETT=True  
+    if ((xm >20 and xm <80) and (ym >350 and ym <380))and MAIN :
+        MAIN=False
+        LEV_I=True   
+    if ((xm >20 and xm <80) and (ym >400 and ym <430))and MAIN :
+        MAIN=False
+        LEV_II=True   
+    if ((xm >20 and xm <80) and (ym >450 and ym <480))and MAIN:
+        MAIN=False
+        LEV_III=True   
+    if ((xm >20 and xm <80) and (ym >500 and ym <530))and MAIN:
+        MAIN=False
+        SCORE=True 
+    if ((xm >20 and xm <80) and (ym >250 and ym <290)) and SETT and set_first:  
+        screen.fill(background)
+        TitleMenu("Screen Size")
+        MainMenu(sizeList )
+        sc_size=True
+        set_first=False
+        f_SEET=True
+        if keys[pygame.K_ESCAPE]:
+            sc_size=False
+            set_first=True
+    if sc_size and xm >0:
+        changeScreenSize(xm,ym)
+        screen.fill(background)
+        TitleMenu("Screen Size")
+        MainMenu(sizeList )
+        if keys[pygame.K_ESCAPE]:
+            sc_size=False
+            set_first=True
+    if ((xm >20 and xm <80) and (ym >300 and ym <330))and SETT and c_first:
+        screen.fill(background)
+        TitleMenu("Background Color")
+        c_first=False
+        if keys[pygame.K_ESCAPE]:
+            c_first=True
+            set_first=True
+
+    if ((xm >20 and xm <80) and (ym >550 and ym <580)) :
+        screen.fill(background)
+        keepScore(456)
+        text=INST_FNT.render("Make sure you update the score file", 1, BLACK)
+        screen.blit(text, (40,200))
+        text=INST_FNT.render("before you exit", 1, BLACK)
+        screen.blit(text, (40,300))
+        text=INST_FNT.render("Thank you for playing", 1, BLACK)
+        screen.blit(text, (40,400))
+        pygame.display.update()
+        pygame.time.delay(50)
+        MAIN=False
+        SCORE=False 
+        pygame.time.delay(3000)
+        check=False
+    pygame.display.update()
+    pygame.time.delay(10)
+
+os.system('cls')
+pygame.quit()
+
+
+
+
